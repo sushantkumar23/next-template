1bda3a8ad2e2c3da5456a6c16364f9fc6a726e01
Sushant Kumar <sushantkumar23@gmail.com>
2023-10-12 10:42:01 +0530

    Added blog pages on the sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@
 
 import { MetadataRoute } from 'next'
 
+// Remove this line if you want to add blog posts to your sitemap
+import { allPosts, Post } from 'contentlayer/generated'
 import { BASE_URL } from '@/config'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -10,5 +12,11 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date().toISOString().split('T')[0]
   }))
 
-  return [...routes]
+  // Remove this part: if you don't want to add blog posts to your sitemap
+  const blogs = allPosts.map((post: Post) => ({
+    url: `${BASE_URL}${post.url}`,
+    lastModified: new Date(post.date).toISOString().split('T')[0]
+  }))
+
+  return [...routes, ...blogs]
 }
